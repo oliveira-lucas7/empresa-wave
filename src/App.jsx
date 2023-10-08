25e23070447fe5ecec42b34fef7ec71498623ee4
oliveira-lucas7 <lucasrodrigoclash038@gmail.com>
2023-10-08 16:05:21 -0300

    Tela da Home feita

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/header";
 import Projetos from "./components/projetos";
+import Home from "./components/home"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>TESTANDO GITHUB</h1>
-      <h1>TESTE BRANCH MASTER</h1>
+      <Home/>
       <Projetos />
       <Footer />
     </div>
---
+++ src/components/home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logohome from "../img/logo-home.png"
+import Style from "../css/home.module.css"
+
+function Home() {
+  return (
+    <main className={Style.container}>
+        <div className={Style.info}>
+            <h1>LOGIC WAVE</h1>
+            <p>Bem-vindo à LogicWave, onde a inovação encontra a tecnologia para criar soluções digitais que impulsionam o sucesso de nossos clientes.</p>
+            <button className={Style.sobre}>Sobre Nós</button>
+        </div>
+        <div className={Style.imagemLogo}>
+            <img className={Style.imagemLogo} src={logohome} alt="Logo da "/>
+        </div>
+    </main>
+  );
+}
+
+export default Home;
