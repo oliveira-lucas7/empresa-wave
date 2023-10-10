043805c59ee218efeb7874cf7458fafe522bb760
tomas-nogueira <tomasmnogueira2006@gmail.com>
2023-10-10 12:04:06 -0300

    Mais ajustes no sobre nos

--- src/components/sobrenos.jsx
+++ src/components/sobrenos.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Style from "../css/sobrenos.module.css";
+import Escudo from "../img/escudo.png"
+import Ideia from "../img/novaideia.png"
+import Computador from "../img/computador.png"
 
 function sobrenos() {
   return (
@@ -7,6 +10,7 @@ function sobrenos() {
       <h2 className={Style.h2}>Sobre a empresa</h2>
       <div className={Style.container}>
         <div className={Style.secondary}>
+          <img src={Ideia} alt="Lampada de ideia" className={Style.img}/>
           <span className={Style.text}>Inovação</span>
           <span className={Style.textmain}>
             A inovação encontra a tecnologia para criar soluções digitais que
@@ -14,23 +18,22 @@ function sobrenos() {
             ao desenvolvimento de sistemas e sites, comprometida em transformar
             ideias em realidade digital.
           </span>
-          <span className={Style.seta}>...</span>
         </div>
         <div className={Style.secondary}>
+          <img src={Escudo} alt="Escudo" className={Style.img}/>
           <span className={Style.text}>Segurança</span>
           <span className={Style.textmain}>
           Na LogicWave, nossa abordagem é simples: entender as necessidades exclusivas de cada cliente e, em seguida, aplicar nossa experiência e criatividade para fornecer soluções sob medida que impulsionem o crescimento e o sucesso.
           </span>
-          <span className={Style.seta}>...</span>
         </div>
         <div className={Style.secondary}>
+          <img src={Computador} alt="Imagem de computador" className={Style.img} />
           <span className={Style.text}>Soluções</span>
           <span className={Style.textmain}>
             Fundada por um grupo de apaixonados por tecnologia, nossa empresa
             nasceu da crença de que a tecnologia pode ser uma força motriz para
             o progresso, eficiência e inovação em todos os setores.
           </span>
-          <span className={Style.seta}>...</span>
         </div>
       </div>
     </div>
