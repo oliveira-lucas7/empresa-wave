4a4f1b6fe9082833d58c92000a8465053ea354fc
rodrigo-seabra <rodrigo.seabra08@outlook.com>
2023-10-12 21:20:56 -0300

    Barra de contatos

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Footer from "./components/Footer";
 import Projetos from "./components/projetos";
 import Header from "./components/header";
 import Carrosel from "./components/carrosel";
-import Sobrenos from "./components/sobrenos";
+import Sobrenos from "./components/Sobrenos";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Carrosel />
+      <Card />
+
       <Projetos />
       <Sobrenos />
       <Footer />
